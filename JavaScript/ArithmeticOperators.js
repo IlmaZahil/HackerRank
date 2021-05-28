50849e144f3ad1ba084abe6046d3f9f6bbb0def2
Ilma Zahil <66563618+IlmaZahil@users.noreply.github.com>
2021-05-28 21:09:48 +0530

    Updated

---
+++ JavaScript/ArithmeticOperators.js
@@ -0,0 +1,35 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the rectangle's area.
+**/
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    var l = parseFloat(length);
+    var w = parseFloat(width);
+    area = l * w;
+    
+    return area;
+}
+
+/**
+*   Calculate the perimeter of a rectangle.
+*	
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*   
+*	Return a number denoting the perimeter of a rectangle.
+**/
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    var l1 = parseFloat(length);
+    var w1 = parseFloat(width);
+    perimeter = 2*(l1+w1);
+    
+    return perimeter;
+}
